18eff84b63f3ed49a0825cfa555689fa09005427
leovoel <leovoel@users.noreply.github.com>
2023-08-24 12:17:16 -0300

    Release 2.1.0
    https://github.com/ultraabox/ultrabox_typescript/commit/3fa41b067d849c01cae7bf1c632e8f542ff448da

---
+++ service_worker.js
@@ -0,0 +1,49 @@
+
+const cacheName = "UltraBox";
+
+self.addEventListener("install", function(event) {
+	event.waitUntil(
+		caches.open(cacheName).then(function(cache) {
+			return cache.addAll([
+				"/",
+				"/beepbox_editor.min.js",
+				"/player/",
+				"/player/beepbox_player.min.js",
+				"/2_4/",
+				"/2_4/beepbox_editor.min.js",
+				"/2_4/player/",
+				"/2_4/player/beepbox_player.min.js",
+				//"/2_3/",
+				//"/2_3/beepbox_editor.min.js",
+				"https://cdn.jsdelivr.net/npm/lamejs@1.2.0/lame.min.js",
+			]).then(() => self.skipWaiting());
+		})
+	);
+});
+
+self.addEventListener("activate", function(event) {
+	event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", function(event) {
+	if (event.request.method != "GET") return;
+	
+	event.respondWith(
+		caches.open(cacheName).then(function(cache) {
+			return fetch(event.request).then(function(response) {
+				// If this is a local resource or related to google fonts, add
+				// it to the permanent cache.
+				if (event.request.url.startsWith(self.location.origin) ||
+					event.request.url.startsWith("https://fonts.googleapis.com") ||
+					event.request.url.startsWith("https://fonts.gstatic.com") ||
+					event.request.url.startsWith("https://cdn.jsdelivr.net"))
+				{
+					cache.put(event.request, response.clone());
+				}
+				return response;
+			}).catch(function() {
+				return cache.match(event.request);
+			});
+		})
+	);
+});
